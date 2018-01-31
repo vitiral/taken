bcf22644ae274ebc90da9eed6a0306717cf7037a
Garrett Berg <googberg@gmail.com>
2018-01-31 16:22:10 -0700

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -11,29 +11,24 @@
 //!
 //! - [/u/CUViper] for poiting out the tradeoffs of this strategy.
 //! - [/u/jasonkdark] for the initial implementation.
+//! - [/u/i_r_witty] for improving this quote
 //!
 //! > _[In the voice of Liam Neeson]_
-//! > _If you let my object go now, that'll be the end of it. But if you don't, I will look for you,
-//! > I will find you, and I will kill you._
-//!
-//! Whoa Liam Neeson, you'll get your object back, I swear!
+//! > But what I do have are a very particular set of macros, macros I have acquired over a very
+//! > long career. Macros that make me a nightmare for closures like you. If you let my variable go
+//! > now, that'll be the end of it. I will not look for you, I will not pursue you. But if you
+//! > don't, I will look for you, I will find you, and I will compile you.
 //!
 //! [/r/rust]: https://www.reddit.com/r/rust/comments/7u29r3/help_me_make_the_own_macro_and_understand_its_use/
 //! [/u/CUViper]: https://www.reddit.com/r/rust/comments/7u29r3/help_me_make_the_own_macro_and_understand_its_use/dthcvlp/
 //! [/u/jasonkdark]: https://www.reddit.com/r/rust/comments/7u29r3/help_me_make_the_own_macro_and_understand_its_use/dthfcnt/
+//! [/u/i_r_witty]: https://www.reddit.com/r/rust/comments/7ubwjv/announcing_the_taken_crate_with_special_thanks_to/dtjrusk/
 
 
 
 /// Take ownership of specific variables.
 ///
-/// One of the main use cases is closures. Closures try to be "smart" about how
-/// much scope they capture. If you don't mutate a variable they take `&var`,
-/// if you do mutate they take `&mut var`. However, if you require ownership
-/// you use `move`, i.e. `move || {... do stuff with var...}`... _right_?
-///
-/// The problem with `move` is that it moves _every_ variable that is referenced. If you only need
-/// to move a few variables it can be a pain. Interestingly, you can tell the compiler to only move
-/// _specific_ variables like so:
+/// You can instruct the compiler on how you want to own your variables in this way:
 ///
 /// ```rust
 /// # let (x, y) = (1, 2);
@@ -42,24 +37,85 @@
 /// // ... etc
 /// ```
 ///
-/// But this is quite silly and not obvious to someone who doesn't know about it. Instead, use the
-/// `take!` macro and your code will be self documenting.
+/// But this is quite silly and not always completely obvious what you are trying to do.
+/// Use the `take!` macro and your code will be self documenting.
 ///
-/// You can also use `take!` to perform a clone or assert to the compiler that you are only taking
-/// a certain kind of reference. See the examples for more.
+/// ```rust
+/// # #[macro_use] extern crate taken;
+/// # fn main() {
+/// # let (x, y) = (1, 2);
+/// take!(x, y); // I see, you are just taking ownership
+/// // ... etc
+/// # }
+/// ```
+///
+/// It also allows you to _change_ the mutability of variables more concisely.
+///
+/// ```rust
+/// # #[macro_use] extern crate taken;
+/// # fn main() {
+/// // v has to be mutable at first
+/// let mut v = vec![1, 2, 3, 4];
+/// v.extend(0..100);
+/// take!(v); // make sure we don't mutate v anymore.
+/// # }
+/// ```
+///
+/// As well as do operations in mass, such as if you want to assert how you own
+/// variables in your closure.
+///
+/// ```rust
+/// # #[macro_use] extern crate taken;
+/// # fn main() {
+/// let (mut w, x, y, z) = (42, vec![1, 2], vec![3,10], vec![4, 5, 6]);
+/// {
+///     let mut closure = || {
+///         // Specify _how_ we are using the outside scope
+///         take!(&mut w, &x, mut y, =mut z);
+///
+///         // We took a a mutable reference to `w`
+///         *w = 777;
+///
+///         // `x` is an immutable reference
+///         println!("we can't change x: {:?}", x);
+///
+///         // `y` is mutable and will be dropped at the end of scope.
+///         y.push(111);
+///
+///         // We took a mutable clone of `z`
+///         z.push(40);
+///     };
+///     closure();
+/// }
+///
+/// println!("w has been mutated: {}", w);
+/// println!("x couldn't change: {:?}", x);
+/// // println!("y was moved: {:?}", y); // ERROR: use of moved value
+/// println!("z was cloned, so it didn't change: {:?}", z);
+/// # }
+/// ```
+///
+/// One of the main use cases is closures. Closures try to be "smart" about how
+/// much scope they capture. If you don't mutate a variable they take `&var`,
+/// if you do mutate they take `&mut var`. However, if you require ownership
+/// you use `move`, i.e. `move || {... do stuff with var...}`... _right_?
+///
+/// The problem with `move` is that it moves _every_ variable that is referenced. If you only need
+/// to move a few variables it can be a pain. Interestingly, you can tell the compiler to only move
+/// _specific_ variables using `let x = x` or `take!(x)`.
 ///
 /// ## Downsides
-/// If you use this macro (or `let x = x`) then the closure becomes `FnOnce`.
+/// If you use this macro (or `let x = x`) inside a closure then it becomes `FnOnce`.
 ///
-/// Unfortunately the best explanation of the trade offs is [currently a reddit
-/// thread][reddit]. Please help flush out these docs more!
+/// Unfortunately the best explanation of the trade offs is [currently a reddit thread][reddit].
+/// Please help flush out these docs more!
 ///
 /// [reddit]: https://www.reddit.com/r/rust/comments/7u29r3/help_me_make_the_own_macro_and_understand_its_use/dthcvlp/
 ///
 /// # Examples
 ///
 /// ## Changing Ownership
-/// It is easy to change the mutability and take references or clones.
+/// It is easy to change the mutability and take references or clones on one or more variables.
 ///
 /// ```rust
 /// # #[macro_use] extern crate taken;
@@ -71,7 +127,7 @@
 ///     c,      // let c = c;
 ///     mut d,  // let mut d = d;
 ///     =e,     // let e = e.clone();
-///     =mut f, // let mut e = e.clone();
+///     =mut f, // let mut f = f.clone();
 /// );
 /// # }
 /// ```
@@ -89,51 +145,12 @@
 ///     var_c as c,      // let c = var_c;
 ///     mut var_d as d,  // let mut d = var_d;
 ///     =var_e as e,     // let e = var_e.clone();
-///     =mut var_f as f, // let mut e = var_e.clone();
+///     =mut var_f as f, // let mut f = var_f.clone();
 /// );
 /// # }
 /// ```
 ///
-/// ## Usecase: Closures
-///
-/// Closures are one of the main use cases.
-///
-/// ```
-/// #[macro_use] extern crate taken;
-///
-/// # fn main() {
-/// // make them all mutable to demo that we can control
-/// // mutability using `take!`
-/// let mut w = vec![1, 2, 3];
-/// let mut x = vec![1, 2, 3];
-/// let mut y = vec![1, 2, 3];
-/// let mut z = vec![10];
-///
-/// {
-///     let closure = || {
-///         take!(&w, &mut x, y, mut z);
-///         // w.push(5); // ERROR: cannot borrow as mutable
-///         x.push(4);    // mutate reference to x
-///         z.push(10);   // we own `mut z`
-///
-///         println!("&x: {:?}", &x);
-///         println!("moved y: {:?}", y);
-///         println!("moved mut z: {:?}", z);
-///         // y and z are dropped
-///     };
-///
-///     closure();
-/// }
-///
-/// println!("&w after: {:?}", w);    // We can still print w!
-/// println!("&x after: {:?}", x);    // We can still print x!
-/// // println!("y after: {:?}", y);  // ERROR: use of moved value
-/// // println!("z after: {:?}", z);  // ERROR: use of moved value
-/// # }
-/// ```
-///
 /// ## Usecase: Threads
-///
 /// Threads are another primary use case, as threads use closures. Threads in particular are always
 /// `FnOnce` and often find themselves cloning and moving specific variables.
 ///
